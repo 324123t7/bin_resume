c60f31892f9b262e3eda1b123b765bc62dbeacb7
董文賓 <a30082424@gmail.com>
2024-09-11 15:51:56 +0800

    new_version

---
+++ js/ajax.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".topicsmenu>button").click(function (e) {
+    $(".topicsmenu>button.selected").removeClass();
+    $(".topicsmenu>button").addClass('btn card-body btn-outline-dark');
+    $(".topicsview").load($(this).addClass("selected").attr("href"));
+    e.preventDefault();
+  }).first().click();
+});
