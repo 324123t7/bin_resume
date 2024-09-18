d242ebd4442e0f7ab3e914e8f03459ec714be606
董文賓 <a30082424@gmail.com>
2024-09-18 13:29:31 +0800

    uptopicsbthref

--- js/ajax.js
+++ js/ajax.js
@@ -7,3 +7,12 @@ $(function () {
   }).first().click();
 });
 
+$(function () {
+  $(".twsigmenu>button").click(function (e) {
+    $(".twsigmenu>button.twselected").removeClass();
+    $(".twsigmenu>button").addClass('btn card-body btn-outline-dark');
+    $(".twsigview").load($(this).addClass("twselected").attr("href"));
+    e.preventDefault();
+  }).first().click();
+});
+
