30ca79e9446cff54926ec458d41cd9eabc8d877c
董文賓 <a30082424@gmail.com>
2024-09-19 21:54:28 +0800

    addcontact

--- js/ajax.js
+++ js/ajax.js
@@ -16,22 +16,37 @@ $(function () {
   }).first().click();
 });
 
-const name=$('#name').val();
 
-var data = {
-  'entry.1107420346': name,
-  'entry.1675490282': 'test',
-  'entry.243514853': 'test',
-  'entry.766702982': 'test',
-  'entry.988081046':'test'
-};
-$.ajax({
-  type: 'POST',
-  url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdexI7qBomUCZgGRQWcyP813sAaZso6JV0iuzRvhZSBbwNEFA/formResponse',
-  data: data,
-  contentType: 'application/json',
-  dataType: 'jsonp',
-  complete: function() {
-    alert('資料已送出！');
-  }
-});
+$("#submail").click(function(){
+  const name=$('#name').val();
+  const call=$('#call').val();
+  const email=$('#email').val();
+  const subject=$('#subject').val();
+  const message=$('#message').val();
+  var data = {
+    'entry.1107420346': name,
+    'entry.1675490282': call,
+    'entry.243514853': email,
+    'entry.766702982': subject,
+    'entry.988081046':message
+  };
+  $.ajax({
+    type: 'POST',
+    url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSdexI7qBomUCZgGRQWcyP813sAaZso6JV0iuzRvhZSBbwNEFA/formResponse',
+    data: data,
+    contentType: 'application/json',
+    dataType: 'jsonp',
+    complete: function() {
+      $('.ok').removeClass('d-none');
+      $('.ok').addClass('d-block');
+      $('.viewback').removeClass('d-none');
+      $('.viewback').addClass('show');
+      $('#close').click(function(){
+        $('.ok').removeClass('d-block');
+        $('.ok').addClass('d-none');
+        $('.viewback').removeClass('d-block');
+        $('.viewback').addClass('d-none');
+      })
+    }
+  });
+})
